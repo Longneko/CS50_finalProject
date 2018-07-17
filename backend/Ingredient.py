3606fa136150ce62c43093206ce02f53cf1e7ee7
Valerii Hulevych <valeriy.gul@gmail.com>
2018-07-17 16:23:00 +0000

    -Renamed Dish to Recipe. -Moved amount related attributes from Ingredient to Recipe into a namedtuple derived class Content. -Renamed Recipe.ingredients to Recipe.contents which will store the Content tuples. -Added id parameters (SQLite DB id) to Ingredient and Recipe.

--- backend/Ingredient.py
+++ backend/Ingredient.py
@@ -2,16 +2,16 @@ class Ingredient(object):
     """An ingredient that is a part of a dish.
 
     :param name: A string, name of the ingredient.
-    :params amt_units: A number, number of measurement units comprising amount of ingredient.
-    :params amt_unit_type: A string, measurement unit used to denote amount of ingredient.
+    :param category: A string, category of the ingredient.
+    :param id: An integer, id of the ingredient in the SQLite DB.
     :param allergies: A set, contains widespread allergies where the ingredient is known
         to be the allergen.
     :param tags: A set, contains arbitrary tags e.g. 'spicy'.
     """
-    def __init__(self, name, amt_units=0, amt_unit_type="kg", allergies=set(), tags=set()):
+    def __init__(self, name, category, id=None, allergies=set(), tags=set()):
         self.name = name
-        self.amt_units = amt_units
-        self.amt_unit_type = amt_unit_type
+        self.category = category
+        self.id = id
         self.allergies = allergies
         self.tags = tags
 
@@ -25,16 +25,3 @@ class Ingredient(object):
         if not value:
             raise RuntimeError("name must be a non-empty string")
         self._name = value
-
-    @property
-    def amt_units(self):
-        """Get amount as number of measurement units"""
-        return self._amt_units
-
-    @amt_units.setter
-    def amt_units(self, value):
-        try:
-            if value and value >= 0:
-                self._amt_units = value
-        except ValueError:
-            raise RuntimeError("amt_units must be a positive number")
